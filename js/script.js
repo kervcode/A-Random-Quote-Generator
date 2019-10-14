a4aa72772ee02980899eb1e1b1acfc931172ae28
Kervintz Noel <10268341+kervcode@users.noreply.github.com>
2019-10-14 19:00:55 -0400

    printQuote : handled citation part

--- js/script.js
+++ js/script.js
@@ -19,88 +19,88 @@ project 1 - A Random Quote Generator
 const quotes = [
   {
     quote : "It’s none of their business that you have to learn to write. Let them think you were born that way.",
-    source: "– Ernest Hemingway"
+    source: " Ernest Hemingway"
   },
   {
     quote : "If you can tell stories, create characters, devise incidents, and have sincerity and passion, it doesn’t matter a damn how you write.",
-    source: "– Somerset Maugham"
+    source: " Somerset Maugham"
   },
   {
     quote : "To produce a mighty book, you must choose a mighty theme.",
-    source: "– Herman Melville"
+    source: " Herman Melville"
   },
   {
     quote : "It took me fifteen years to discover I had no talent for writing, but I couldn’t give it up because by that time I was too famous.",
-    source: "– Robert Benchley"
+    source: " Robert Benchley"
   },
   {
     quote : "Tell your men they work for me now, this is my city!",
-    source : "The Dark Knight",
+    source : " The Dark Knight",
     year: 2008
   },
   {
     quote : "If you have other things in your life—family, friends, good productive day work—these can interact with your writing and the sum will be all the richer.",
-    source: "– David Brin"
+    source: " David Brin"
   },
   {
     quote : "Life is what happens to you while you’re busy making other plans.",
-    source: "– John Lennon"
+    source: " John Lennon"
   },
   {
     quote : "The most common way people give up their power is by thinking they don’t have any.",
-    source: "– Alice Walker"
+    source: " Alice Walker"
   },
   {
     quote : "The mind is everything. What you think you become.",
-    source: "– Buddha"
+    source: " Buddha"
   },{
     quote : "Momma always said life was like a box of chocolates. You never know what you're gonna get.",
-    source: "Forrest Gump",
+    source: " Forrest Gump",
     year: 1994
   },
   {
     quote : "The best time to plant a tree was 20 years ago. The second best time is now. –Chinese Proverb",
-    source: "– Chinese Proverb"
+    source: " Chinese Proverb"
   },
   {
     quote : "An unexamined life is not worth living.",
-    source: "– Socrates"
+    source: " Socrates"
   },
   {
     quote : "Your time is limited, so don’t waste it living someone else’s life.",
-    source: "– Steve Jobs"
+    source: " Steve Jobs"
   },
   {
     quote : "I am not a product of my circumstances. I am a product of my decisions.",
-    source: "– Stephen Covey"
+    source: " Stephen Covey"
   },
   {
     quote : "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
-    source: "– Maya Angelou"
+    source: " Maya Angelou"
   },
   {
     quote : "Either you run the day, or the day runs you.",
-    source: "- Jim Rohn"
+    source: " Jim Rohn"
   },
   {
     quote : "The two most important days in your life are the day you are born and the day you find out why.",
-    source: "– Mark Twain"
+    source: " Mark Twain"
   },
   {
     quote : "Everything you’ve ever wanted is on the other side of fear.",
-    source: "– George Addair"
+    source: " George Addair"
   },
   {
     quote : "Believe you can and you’re halfway there.",
-    source: "– Theodore Roosevelt"
+    source: " Theodore Roosevelt"
   },
   {
     quote : 'Teach thy tongue to say, \"I do not know,\" and thous shalt progress.',
-    source: "– Maimonides"
+    source: " Maimonides"
   },
   {
     quote : "Happiness is not something readymade.  It comes from your own actions.",
-    source: "– Dalai Lama"
+    source: " Dalai Lama"
   }
 ]
 console.log('Number of quotes: ' + quotes.length);
@@ -139,12 +139,20 @@ function getRandomQuote(){
 function printQuote() {
   var storeRandomQuote = getRandomQuote(); //will the value to get the random quotes
   var html = ''; //
-  //assign quote with quote array index equivalent to the random number
-  var randomQuote = '<p class="quote">' + quotes[storeRandomQuote].quote + '</p>';
-  //assign source with quote array index equivalent to the random number
-  var randomSource = '<p class="source">' + quotes[storeRandomQuote].source + '</p>';
-  // check if quote is a citation
+  ;
+  // check if quote is a citation 
       // if has property year: it is a citation
+        if(quotes[storeRandomQuote].year) {
+          //assign quote with quote array index equivalent to the random number
+          var randomQuote = '<p class="quote">' + quotes[storeRandomQuote].quote + '</p>';
+          //assign source with quote array index equivalent to the random number
+          var randomSource = '<p class="source">' + quotes[storeRandomQuote].source + '<span class="year">'+ quotes[storeRandomQuote].year + '</span></p>'
+        } else {
+          //assign quote with quote array index equivalent to the random number
+          var randomQuote = '<p class="quote">' + quotes[storeRandomQuote].quote + '</p>';
+          //assign source with quote array index equivalent to the random number
+          var randomSource = '<p class="source">' + quotes[storeRandomQuote].source + '</p>'
+        }
           // format html variable to print citation
       // else it is not citation
           // format htmlvariable to print regular quote
