1254f032620c6dcb6ea3378d87af9ea88c6d7a04
Kervintz Noel <10268341+kervcode@users.noreply.github.com>
2019-10-14 22:47:45 -0400

    add random background functionality

--- js/script.js
+++ js/script.js
@@ -3,18 +3,6 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
 // Step 1: build a collection of quotations
 const quotes = [
   {
@@ -103,12 +91,6 @@ const quotes = [
     source: " Dalai Lama"
   }
 ]
-console.log('Number of quotes: ' + quotes.length);
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
 
 // create getRandomQuote function
 function getRandomQuote(){
@@ -120,23 +102,22 @@ function getRandomQuote(){
   return randomNumber;
 }
 
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
+function randomBgColor() {
+  // source : https://stackoverflow.com/questions/1484506/random-color-generator
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    var bgColor = document.getElementById('body').style.backgroundColor = color;
+    var btnColor = document.getElementById('loadQuote').style.backgroundColor = color;
+    return bgColor;
+} 
 
 // create printQuote function
 function printQuote() {
+  // call randomBgColor
+  randomBgColor()
   var storeRandomQuote = getRandomQuote(); //will the value to get the random quotes
   var html = ''; //
   ;
@@ -166,14 +147,4 @@ function printQuote() {
   console.log(getRandomQuote());
 }
 
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
