63192e34b1441aa9988c474dc943c6c0829c63a1
Kervintz Noel <10268341+kervcode@users.noreply.github.com>
2019-10-19 10:42:29 -0400

    printQuote function rewritten

--- js/script.js
+++ js/script.js
@@ -10,18 +10,19 @@ const quotes = [
     quote : "But man is not made for defeat,\" he said. \"A man can be destroyed but not defeated.",
     source: " Ernest Hemingway",
     citation: "The Old Man and the Sea",
-    year: 1952
+    year: '1952'
   },
   {
     quote : "To produce a mighty book, you must choose a mighty theme.",
     source: " Herman Melville",
-    citation: "Moby Dick"
+    citation: "Moby Dick",
+    year: '1851'
   },
   {
-    citation : "Tell your men they work for me now, this is my city!",
-    source : " The Joker - Heath Ledger",
-    Citation: " The Dark Knight",
-    year: 2008
+    quote : "Tell your men they work for me now, this is my city!",
+    source : " The Dark Knight",
+    citation: " The Joker",
+    year: '2008'
   },
   {
     quote : "The most common way people give up their power is by thinking they don’t have any.",
@@ -31,9 +32,10 @@ const quotes = [
     quote : "The mind is everything. What you think you become.",
     source: " Buddha"
   },{
-    citation : "Momma always said life was like a box of chocolates. You never know what you're gonna get.",
-    source: " Forrest Gump",
-    year: 1994
+    quote : "Momma always said life was like a box of chocolates. You never know what you're gonna get.",
+    source: " Tom Hanks",
+    citation: "Forrest Gump",
+    year: '1994'
   },
   {
     quote : "Your time is limited, so don’t waste it living someone else’s life.",
@@ -98,15 +100,32 @@ function printQuote() {
   var storeRandomQuote = getRandomQuote(); 
   var html = ''; 
       // concatenate html with quote and source - this will print for any array
+          html += '<p class="quote">'+ quotes[storeRandomQuote].quote + '</p>';
       // check which property exist in the object
           // if citation and year exist
+              if (quotes[storeRandomQuote].citation && quotes[storeRandomQuote].year){
+                html += '<p class="source">' + 
+                              quotes[storeRandomQuote].source + 
+                        '<span class= "citation"> ' + 
+                              quotes[storeRandomQuote].citation + 
+                        '</span>' + 
+                        '<span class= "year"> ' + quotes[storeRandomQuote].year + '</span></p>';
+                console.log(html);
+                console.log(' ');
+                console.log(quotes[storeRandomQuote].citation);
+              }
               // concatenate HTML with property value for citation and year
           // else if only citation
               // concatenate HTML with property value for citation
+              else if (quotes[storeRandomQuote].citation) {
+                html += '<p class="source">' + quotes[storeRandomQuote].source + '<span class= "citation">, ' + quotes[storeRandomQuote].citation + '</span></p>';
+              } else
           // else if only year
-              // concatenate HTML with property value for year
-
+              // concatenate HTML with property value for source
+                  html += '<p class="source">' + quotes[storeRandomQuote].source + '</p>';
   // return html
+  // console.log(html);
+    return document.getElementById('quote-box').innerHTML = html;
 }
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
